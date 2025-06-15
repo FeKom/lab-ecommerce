5df153a47788e93c5e20f4df5a97fd874093f4ad
fekom <fekomninakis@gmail.com>
2025-06-15 11:31:04 -0300

    feat: creating product domain

--- catalog/src/main/java/github/fekom/catalog/domain/entities/Product.java
+++ catalog/src/main/java/github/fekom/catalog/domain/entities/Product.java
@@ -1,4 +1,37 @@
 package github.fekom.catalog.domain.entities;
 
-public class Product {
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+//entidade do sistema, desacoplado de framework
+public record Product(String id,
+                      String name,
+                      BigDecimal price,
+                      int stock,
+                      Instant createAt,
+                      Instant updateAt,
+                      List<String> tags,
+                      Optional<String> category,
+                      Optional<String> description) {
+
+    public static Product create(String name,
+                                 BigDecimal price,
+                                 int stock,
+                                 List<String> tags,
+                                 Optional<String> category,
+                                 Optional<String> description) {
+
+        if(price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("Price must be greater than zero");
+        }
+        if(stock < 0) {
+            throw new IllegalArgumentException("Stock must be greater than zero");
+        }
+        return new Product(UUID.randomUUID().toString(), name, price, stock, Instant.now(), Instant.now(), tags, category, description);
+    }
+
 }
