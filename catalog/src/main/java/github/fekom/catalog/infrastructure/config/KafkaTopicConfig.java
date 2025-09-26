fdfacc4e4164220013b41fb4ffd35ad8caef7d43
fekom <fekomninakis@gmail.com>
2025-09-26 15:58:41 -0300

    feat

--- catalog/src/main/java/github/fekom/catalog/infrastructure/config/KafkaTopicConfig.java
+++ catalog/src/main/java/github/fekom/catalog/infrastructure/config/KafkaTopicConfig.java
@@ -13,15 +13,6 @@ import org.springframework.kafka.core.KafkaAdmin;
 @Configuration
 public class KafkaTopicConfig {
 
-    @Value(value = "${spring.kafka.bootstrap-servers}")
-    private String bootstrapAddress;
-
-    @Bean
-    public KafkaAdmin kafkaAdmin() {
-        Map<String, Object> configs = new HashMap<>();
-        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        return new KafkaAdmin(configs);
-    }
 
     @Bean
     public NewTopic productSearchProcessedTopic() {
