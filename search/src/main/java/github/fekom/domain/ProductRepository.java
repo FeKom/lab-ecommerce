24ddf701524d30dd6f6c72c4901450029a86c8e3
fekom <fekomninakis@gmail.com>
2025-11-25 22:21:55 -0300

    feat: created all infrastructure for search microservice

--- search/src/main/java/github/fekom/domain/ProductRepository.java
+++ search/src/main/java/github/fekom/domain/ProductRepository.java
@@ -1,26 +1,15 @@
 package github.fekom.domain;
 
-
-import org.hibernate.id.uuid.UuidVersion7Strategy;
-
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
+import java.util.UUID;
 
 public interface ProductRepository {
-    void save(List<Product> products );
-    default void save(Product product){
-        save(List.of(product));
-    }
-
-    List<Product> find(ProductQuery query);
-
-    default List<Product> findAll(){
-        return find(new ProductQuery.Builder().build());
-    }
-    default Optional<Product> findById(UuidVersion7Strategy id) {
-        return find(new ProductQuery.Builder().ids(Set.of(id)).build()).stream().findFirst();
-    }
-
+    Product save(Product product);
+    Optional<Product> findById(UUID id);
+    List<Product> findAll();
+    List<Product> findByName(String name);
+    void update(Product product);
+    void delete(UUID id);
+    boolean existsById(UUID id);
 }
-
